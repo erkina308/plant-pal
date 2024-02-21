52c9170e2b39cdba4ba64e64bef1f16cf2536fb6
Mohammed Sajjadul <mohammedsajjadul@gmail.com>
2024-02-21 16:28:16 +0000

    alahslas

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const app = express();
-const { connectDb } = require("./connection");
 const apiRouter = require("./routes/apiRouter");
 app.use(express.json());
 
 app.use("/api", apiRouter);
-connectDb();
-// Error handling middleware
-app.use((err, req, res, next) => {
-  if (err instanceof MongooseError) {
-    // Mongoose timeout error
-    console.error("Mongoose timeout error:", err.message);
-    res.status(500).send("Database operation timed out");
-  } else {
-    // Other types of errors
-    console.error(err.stack);
-    res.status(500).send("Something went wrong");
-  }
-});
 
 app.use((err, req, res, next) => {
   if (err.status === 404) {
@@ -45,10 +31,10 @@ app.use((err, req, res, next) => {
 //   next(err);
 // });
 
-app.use((err, req, res, next) => {
-  if (err.name === "MongoServerError" && err.code === 11000) {
-    next(new Error("There was a duplicate key error"));
-  }
-  next(err);
-});
+// app.use((err, req, res, next) => {
+//   if (err.name === "MongoServerError" && err.code === 11000) {
+//     next(new Error("There was a duplicate key error"));
+//   }
+//   next(err);
+// });
 module.exports = app;
--- connection.js
+++ connection.js
@@ -4,16 +4,14 @@ const express = require("express");
 require("dotenv").config({
   path: `${__dirname}/.env.production`,
 });
-function connectDb() {
-  const mongoString = process.env.DATABASE_URL;
-  mongoose
-    .connect(mongoString, { bufferCommands: false })
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-      console.error("MongoDB connection error:", err.message);
-    });
-}
+const mongoString = process.env.DATABASE_URL;
+mongoose
+  .connect(mongoString)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+  });
 
-module.exports = { mongoose, connectDb };
+module.exports = mongoose;
